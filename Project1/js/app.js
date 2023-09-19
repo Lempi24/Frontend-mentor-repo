6791e594ae5060b8382cc42d60ec45b1a823c09b
Lempi24 <ShadowHunter69@wp.pl>
2023-09-19 20:49:06 +0200

    Project 1 Results summary

---
+++ Project1/js/app.js
@@ -0,0 +1,31 @@
+let id = 1;
+fetch('data.json')
+	.then((response) => response.json())
+	.then((data) => {
+		data.forEach((item) => {
+			const summaryComponent = document.getElementById(`${id}`);
+
+			const divElement = document.createElement('div');
+			divElement.className = 'badge';
+
+			const imgElement = document.createElement('img');
+			imgElement.src = item.icon;
+
+			const bElement = document.createElement('b');
+			bElement.textContent = item.category;
+
+			const b2Element = document.createElement('b');
+			b2Element.textContent = `${item.score} / 100`;
+
+			divElement.appendChild(imgElement);
+			divElement.appendChild(bElement);
+
+			summaryComponent.appendChild(divElement);
+			summaryComponent.appendChild(b2Element);
+
+			id++;
+		});
+	})
+	.catch((error) => {
+		console.error('Błąd podczas pobierania danych:', error);
+	});
